89dc43d5adee02617701c1c69fd73355220b2267
sderkaoui <sderkaoui>
2025-04-14 15:50:12 +0200

    fix droits

--- cron.sh
+++ cron.sh
@@ -11,6 +11,6 @@ sudo systemctl enable crond --now
 # Ajouter une tâche crontab pour l'utilisateur 'packer'
 echo "[+] Ajout du cronjob..."
 # On récupère la crontab actuelle, ajoute la tâche, et la recharge
-(crontab -l -u packer 2>/dev/null; echo "0 10 * * * bash /home/packer/python-veille-techno/update_feeds.sh") | crontab -u packer -
+sudo bash -c '(crontab -l -u packer 2>/dev/null; echo "0 10 * * * bash /home/packer/python-veille-techno/update_feeds.sh") | crontab -u packer -'
 
 echo "[✅] Installation terminée. Le script sera exécuté toutes les 5 minutes."
